ec9541a0ea47d32da6e9db1b56213830c7f43b99
wwq <wwquen3568@naver.com>
2022-05-27 23:33:36 +0900

    p.195

--- single_pages/views.py
+++ single_pages/views.py
@@ -1,3 +1,15 @@
+from http.client import REQUEST_TIMEOUT
 from django.shortcuts import render
 
 # Create your views here.
+def landing(request):
+    return render(
+        request,
+        'single_pages/landing.html'
+    )
+
+def about_me(request):
+    return render(
+        request,
+        'single_pages/aobut_me.html'
+    )
