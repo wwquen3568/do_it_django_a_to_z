f84f71f6c1417756e11537b3d1ad34bc7a59a7a7
wwq <wwquen3568@naver.com>
2022-05-28 00:22:18 +0900

    248

--- blog/models.py
+++ blog/models.py
@@ -1,10 +1,12 @@
 from distutils.command.upload import upload
 from tokenize import blank_re
 from django.db import models
+import os
 
 # Create your models here.
 class Post(models.Model):
     title = models.CharField(max_length=30)
+    hook_text = models.CharField(max_length=100, blank=True)
     content = models.TextField()
     
     head_image = models.ImageField(upload_to='blog/images/%Y/%m/%d/', blank=True)
@@ -20,3 +22,8 @@ class Post(models.Model):
     def get_absolute_url(self):
         return f'/blog/{self.pk}/'
     
+    def get_file_name(self):
+        return os.path.basename(self.file_upload.name)
+        
+    def get_file_ext(self):
+        return self.get_file_name().split('.')[-1]
