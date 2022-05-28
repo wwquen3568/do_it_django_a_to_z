ee10956f81aca808035aaa07e2817af7de7d9bb4
wwq <wwquen3568@naver.com>
2022-05-28 15:13:01 +0900

    p.371

--- blog/models.py
+++ blog/models.py
@@ -1,8 +1,35 @@
 from distutils.command.upload import upload
 from tokenize import blank_re
 from django.db import models
+from django.contrib.auth.models import User
 import os
 
+class Tag(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    slug = models.SlugField(max_length=200, unique=True, allow_unicode=True)
+    
+    def __str__(self):
+        return self.name
+    
+    def get_absolute_url(self):
+        return f'/blog/tag/{self.slug}/'
+    
+
+class Category(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    slug = models.SlugField(max_length=200, unique=True, allow_unicode=True)
+    
+    def __str__(self):
+        return self.name
+    
+    def get_absolute_url(self):
+        return f'/blog/category/{self.slug}/'
+    
+    
+    class Meta:
+        verbose_name_plural = 'Categories'
+        
+        
 # Create your models here.
 class Post(models.Model):
     title = models.CharField(max_length=30)
@@ -14,10 +41,16 @@ class Post(models.Model):
     
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    
     #author: 추후 작성 예정
+    author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+    
+    category = models.ForeignKey(Category, null=True, blank=True, on_delete=models.SET_NULL)
+    
+    tags =models.ManyToManyField(Tag, blank=True)
     
     def __str__(self):
-        return f'[{self.pk}] {self.title}'
+        return f'[{self.pk}] {self.title} :: {self.author}'
     
     def get_absolute_url(self):
         return f'/blog/{self.pk}/'
@@ -27,3 +60,4 @@ class Post(models.Model):
         
     def get_file_ext(self):
         return self.get_file_name().split('.')[-1]
+    
