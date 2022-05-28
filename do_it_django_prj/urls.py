9da59b0d31e54e0c9beb067dbd00924a691fef39
wwq <wwquen3568@naver.com>
2022-05-28 17:10:30 +0900

    p.432까지 완료 끝

--- do_it_django_prj/urls.py
+++ do_it_django_prj/urls.py
@@ -23,7 +23,6 @@ urlpatterns = [
     path('', include('single_pages.urls')),
     path('admin/', admin.site.urls),
     path('blog/', include('blog.urls')),
-    
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
